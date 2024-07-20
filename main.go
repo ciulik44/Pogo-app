97da1997a22446ac7d3c43544c7881859f931fde
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-07-20 18:43:15 +0200

    postgres credenials fix

--- main.go
+++ main.go
@@ -4,22 +4,21 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	DB_USER     = "postgres"
-	DB_PASSWORD = "password"
-	DB_NAME     = "mydb"
-	DB_HOST     = "db"
-	DB_PORT     = "5432"
-)
-
 func main() {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
 	dbinfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME)
+		dbHost, dbPort, dbUser, dbPassword, dbName)
 	db, err := sql.Open("postgres", dbinfo)
 	if err != nil {
 		log.Fatal(err)
